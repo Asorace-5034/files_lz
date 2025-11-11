1a6740bf10a0d0e421fb7e306f5b05081de2bd13
Asorace-5034 <tafytoo@gmail.com>
2025-11-11 13:24:31 +0300

    Add files via upload

---
+++ lion.py
@@ -0,0 +1,28 @@
+import docx
+import string
+
+
+
+doc = docx.Document('lion.docx')
+
+
+text = []
+all_text = ''
+stat = {}
+
+for paragraph in doc.paragraphs:
+    text.append(paragraph.text)             # получаем массив строк, в которых находятся обзацы
+print('\n'.join(text))
+
+for i in text:                              # создаем один единый текст в виде одной строки
+    all_text += i
+
+text = all_text.split()                     # делим на слова
+
+for i in text:
+    if i in stat:
+        stat[i] += 1
+    else:
+        stat.update("i: 1")
+
+
